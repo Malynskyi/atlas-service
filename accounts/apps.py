659a8df4c3bed7618ec87e95c70bbeadb8d6e4d0
Dmytro <dimamalinskij671@gmail.com>
2026-06-02 15:36:10 +0200

    Auto create admin user on startup

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,5 +1,26 @@
+import os
+
 from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "accounts"
+
+    def ready(self):
+        if os.environ.get("RUN_MAIN") == "true":
+            return
+
+        try:
+            from django.contrib.auth import get_user_model
+
+            User = get_user_model()
+
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="dm",
+                    email="dadima887@gmail.com",
+                    password="55",
+                )
+        except Exception:
+            pass
--- atlas/settings.py
+++ atlas/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_spectacular",
-    "accounts",
+    "accounts.apps.AccountsConfig",
     "warehouse",
 ]
 
